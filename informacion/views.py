f98e504d709ffd1373c8c496fadbf8cac6e8285b
Jorgecuenca1 <jorge.cuenca@unillanos.edu.co>
2022-11-04 21:00:57 -0500

    up html

--- informacion/views.py
+++ informacion/views.py
@@ -5,3 +5,158 @@ from django.shortcuts import render
 def index(request):
 
     return render(request,'index.html')
+
+def about(request):
+
+    return render(request,'about.html')
+def about1(request):
+
+    return render(request,'about-element-1.html')
+
+def about2(request):
+
+    return render(request,'about-element-2.html')
+def about3(request):
+
+    return render(request,'about-element-3.html')
+def about4(request):
+
+    return render(request,'about-element-4.html')
+def about5(request):
+
+    return render(request,'about-element-5.html')
+def about6(request):
+
+    return render(request,'about-element-6.html')
+def about7(request):
+
+    return render(request,'about-element-7.html')
+def advertising(request):
+
+    return render(request,'advertising-marketing.html')
+def blog(request):
+
+    return render(request,'blog.html')
+def blog2(request):
+
+    return render(request,'blog-2.html')
+def blogdetails(request):
+
+    return render(request,'blog-details.html')
+def chooseus1(request):
+
+    return render(request,'chooseus-element-1.html')
+def chooseus2(request):
+
+    return render(request,'chooseus-element-2.html')
+def chooseus3(request):
+
+    return render(request,'chooseus-element-3.html')
+def chooseus4(request):
+
+    return render(request,'chooseus-element-4.html')
+def contact(request):
+
+    return render(request,'contact.html')
+def error(request):
+
+    return render(request,'error.html')
+def faq(request):
+
+    return render(request,'faq.html')
+def financials(request):
+
+    return render(request,'financials-banking.html.html')
+def grid(request):
+
+    return render(request,'grid.html')
+def gridlist(request):
+
+    return render(request,'grid_list.html')
+def healthcaremedicine(request):
+
+    return render(request,'healthcare-medicine.html')
+def mediaentertainment(request):
+
+    return render(request,'media-entertainment.html')
+def newselement1(request):
+
+    return render(request,'news-element-1.html')
+def newselement2(request):
+
+    return render(request,'news-element-2.html')
+def newselement3(request):
+
+    return render(request,'news-element-3.html')
+def newselement4(request):
+
+    return render(request,'news-element-4.html')
+def pricing(request):
+
+    return render(request,'pricing.html')
+def project(request):
+
+    return render(request,'project.html')
+def project2(request):
+
+    return render(request,'project-2.html')
+def project3(request):
+
+    return render(request,'project-3.html')
+def projectdetails(request):
+
+    return render(request,'project-details.html')
+def service(request):
+
+    return render(request,'service.html')
+def serviceelement1(request):
+
+    return render(request,'service-element-1.html')
+def serviceelement2(request):
+
+    return render(request,'service-element-2.html')
+def serviceelement2(request):
+
+    return render(request,'service-element-1.html')
+def serviceelement3(request):
+
+    return render(request,'service-element-3.html')
+def serviceelement4(request):
+
+    return render(request,'service-element-4.html')
+def serviceelement5(request):
+
+    return render(request,'service-element-5.html')
+def skillselement1(request):
+
+    return render(request,'skills-element-1.html')
+def skillselement2(request):
+
+    return render(request,'skills-element-2.html')
+def team(request):
+
+    return render(request,'team.html')
+def teamelement1(request):
+
+    return render(request,'team-element-1.html')
+def teamelement2(request):
+
+    return render(request,'team-element-2.html')
+def teamelement3(request):
+
+    return render(request,'team-element-3.html')
+def teamelement4(request):
+
+    return render(request,'team-element-4.html')
+def trasportationlogistics(request):
+
+    return render(request,'trasportation-logistics.html')
+def travelhospitality(request):
+
+    return render(request,'travel-hospitality.html')
+def workingelement1(request):
+
+    return render(request,'working-element-1.html')
+def workingelement2(request):
+
+    return render(request,'working-element-2.html')
